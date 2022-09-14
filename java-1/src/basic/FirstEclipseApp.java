6c8a274b8b62b4eb0b7629bd1c48f5d178854826
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-14 18:03:51 +0900

    Delete FirstEclipseApp.java

--- java-1/src/basic/FirstEclipseApp.java
+++
@@ -1,9 +0,0 @@
-package basic;
-
-public class FirstEclipseApp {
-	public static void main(String[] args) {
-		System.out.print("이클립스로 만든 첫번째 ");
-		System.out.println("프로그램입니다.");
-		System.out.println("열심히 해봐요!!!");
-	}
-}
