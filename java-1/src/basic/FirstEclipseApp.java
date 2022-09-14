4a7eb48b02a0bc797e3e0fb418b6f178cda9dcb5
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-14 18:40:29 +0900

    test

---
+++ java-1/src/basic/FirstEclipseApp.java
@@ -0,0 +1,16 @@
+package basic;
+
+/*
+프로그램명 : FirstEclipseApp
+작성자 : 홍길동
+작성일자 : 2022년 09월 14일
+*/
+
+public class FirstEclipseApp {
+	public static void main(String[] args) {
+		//출력 메소드 호출
+		System.out.print("이클립스로 만든 첫번째 "); 
+		System.out.println("프로그램입니다.");
+		System.out.println("열심히 해봐요!!!");
+	}
+}
