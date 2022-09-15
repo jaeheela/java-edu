1eae61d4b3a658b743dda067babb33a53ec8a7d0
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-15 12:34:58 +0900

    1Day

--- java-1/src/basic/FirstEclipseApp.java
+++ java-1/src/basic/FirstEclipseApp.java
@@ -6,11 +6,16 @@ package basic;
 작성일자 : 2022년 09월 14일
 */
 
+//행복사 : [Ctrl]+[Alt]+[↑] 또는 [↓]
+//행이동 : [Alt]+[↑] 또는 [↓]
+
+/** 이클립스로 만든 첫번째 클래스 */
 public class FirstEclipseApp {
+	/** JVM에 의해 자동 호출되는 특별한 메소드 */
 	public static void main(String[] args) {
 		//출력 메소드 호출
-		System.out.print("이클립스로 만든 첫번째 "); 
-		System.out.println("프로그램입니다.");
+		System.out.print("이클립스로 만든 첫번째 ");//행주석
+		System.out.println("프로그램입니다.");/* 범위주석 */
 		System.out.println("열심히 해봐요!!!");
 	}
 }
