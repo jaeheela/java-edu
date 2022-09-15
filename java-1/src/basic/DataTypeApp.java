324e65d8a52cb4f43ba4fd973aa43cdfb52acca7
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-15 17:42:45 +0900

    2Day

--- java-1/src/basic/DataTypeApp.java
+++ java-1/src/basic/DataTypeApp.java
@@ -11,17 +11,63 @@ package basic;
 
 public class DataTypeApp {
 	public static void main(String[] args) {
-		//정수형(IntegerType) : 소숫점이 없는 숫자값  
+		//정수형(IntegerType) 리터럴 : 소숫점이 없는 숫자값  
 		// => Java의 정수형 리터럴은 기본적으로 4Byte(Interger)로 표현 >> -2147483648~2147483647
 		System.out.println("<<정수형(IntegerType)>>");
 		System.out.println("정수값(10진수 100) = "+100);//숫자값 >> 10진수 정수값을 표현한 리터럴
-		//println 메소드는 무조건 10진수의 정수값으로 변환하여 출력 처리
+		//println() 메소드는 무조건 10진수의 정수값으로 변환하여 출력 처리
 		System.out.println("정수값(8진수 100) = "+0100);//0숫자값 >> 8진수 정수값을 표현한 리터럴
 		System.out.println("정수값(16진수 100) = "+0x100);//0x숫자값 >> 16진수 정수값을 표현한 리터럴
 		System.out.println("정수값(10진수 100) = "+100L);//숫자값L >> 8Byte(Long)의 정수값을 표현한 리터럴
 		
-		//정수값을 표현하기 위한 자료형 - byte(1Byte), short(2Byte), int(4Byte), long(8Byte)
+		System.out.println("정수값 = "+2147483647);
+		//4Byte로 표현 가능한 정수값 = -2147483648~2147483647
+		// => 4Byte로 표현 불가능한 정수값을 사용할 경우 에러 발생
+		//System.out.println("정수값 = "+2147483648);
+		//4Byte로 표현 불가능한 정수값은 정수값 뒤에 [L]를 붙여 8Byte 정수값으로 표현하여 사용
+		System.out.println("정수값 = "+2147483648L);
 		
+		//정수값을 표현하기 위한 자료형 - byte(1Byte), short(2Byte), int(4Byte), long(8Byte)
+		byte a1=127;//1Byte : -128~127
+		short a2=32767;//2Byte : -32768~32767 
+		int a3=2147483647;//4Byte : -2147483648~2147483647
+		long a4=2147483648L;//8Byte
+		
+		System.out.println("a1 = "+a1);
+		System.out.println("a2 = "+a2);
+		System.out.println("a3 = "+a3);
+		System.out.println("a4 = "+a4);
+		
+		//int a5=100L;//불가능 : 값에 대한 변형이 발생될 수 있으므로 에러 발생
+		long a5=100;//가능
+		System.out.println("a5 = "+a5);
+		System.out.println("==========================================================");
+		//실수형(DoubleType) 리터럴 : 소숫점이 있는 숫자값
+		// => Java의 실수형 리터럴은 기본적으로 8Byte(Double)로 표현
+		System.out.println("<<실수형(IntegerType)>>");
+		System.out.println("실수값(4Byte) = "+12.3F);//실수값F >> 4Byte(Float)의 실수값을 표현한 리터럴 
+		System.out.println("실수값(8Byte) = "+12.3);//실수값 >> 8Byte(Double)의 실수값을 표현한 리터럴
+		//println() 메소드는 아주 작은 실수값 또는 아주 커다란 실수값을 지수형태로 변환하여 출력
+		System.out.println("실수값(8Byte) = "+0.000000000123);
+		//실수값을 지수형태로 표현하여 사용 가능
+		System.out.println("실수값(8Byte) = "+1.23E10);
+		
+		//실수값을 표현하기 위한 자료형 - float(4Byte), double(8Byte)
+		float b1=1.23456789F;//정밀도 : 7자리
+		double b2=1.23456789;//정밀도 : 15자리
+		
+		System.out.println("b1 = "+b1);
+		System.out.println("b2 = "+b2);
+		System.out.println("==========================================================");
+		//문자형(CharacterType) 리터럴 : ' ' 안에 하나의 문자로 표현
+		// => Java의 문자형 리터럴은 기본적으로 2Byte(Double)로 표현
+		// => 일반 문자와 회피 문자로 구분하여 사용
+		//회피 문자(Escape Character) : 표현 불가능한 문자를 표현하기 위해 방법 - \ 기호 사용
+		// => '\n' : Enter , '\t' : Tab, \' : 문자 ', \" : 문자 ", \\ : 문자 \, '\0' : NULL 문자
+		//문자형 리터럴은 내부적으로 약속된 정수값(0~65535)으로 표현 : 유니코드(UniCode)
+		// => 'A' : 65, 'a' : 97, '0' : 48, ' ' : 32, '\n' : 13, '가' : 44032 - 완성형
+		System.out.println("<<문자형(CharacterType)>>");
+
 	}
 }
 
