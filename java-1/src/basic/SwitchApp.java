cf6d3e986cdd8d25eff4890a1ce770c4b7fc204d
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-19 15:02:11 +0900

    4Day

--- java-1/src/basic/SwitchApp.java
+++ java-1/src/basic/SwitchApp.java
@@ -10,6 +10,7 @@ package basic;
 //      }
 // => 비교대상에는 변수 또는 연산식을 작성하여 비교하고자 하는 값을 제공
 // => switch의 값을 case에 작성된 값과 비교하여 같은 경우 해당 위치로부터 작성된 모든 명령 실행
+// => case의 값은 반드시 리터럴만 사용 가능하며 case의 값을 중복 선언하면 에러 발생
 // => 모든 비교값이 다른 경우 default에 작성된 명령 실행 - 생략 가능
 // => break 구문을 만나면 switch 구문 강제 종료
 
@@ -67,6 +68,40 @@ public class SwitchApp {
 			System.out.println("화성으로 이동합니다.");	
 			break;
 		}
+		System.out.println("===============================================================");
+		int jumsu=85;
+		
+		//변수값이 0~100 범위의 유효값인지 아닌지를 구분하여 출력 - 입력값에 대한 유효성 검증
+		if(jumsu>=0 && jumsu<=100) {
+			//System.out.println("[결과]0~100 범위의 정상적인 값이 입력 되었습니다.");
+			
+			//변수값으로 등급을 구분하여 출력
+			//100~90 : A, 89~80 : B, 79~70 : C, 69~60 : D, 59~0 : F
+			String grade="";
+			switch(jumsu/10) {
+			case 10: 
+			case 9: grade="A"; break;
+			case 8: grade="B"; break;
+			case 7: grade="C"; break;
+			case 6: grade="D"; break;
+			default: grade="F";
+			}
+			
+			System.out.println("[결과]"+jumsu+"점 >> "+grade+"학점");
+		} else {
+			System.out.println("[에러]0~100 범위을 벗어난 비정상적인 값이 입력 되었습니다.");
+		}
+		System.out.println("===============================================================");
+		String kor="둘";
+		String eng="";
+		
+		switch (kor) {
+		case "하나": eng="One";	break;
+		case "둘": eng="Two";	break;
+		case "셋": eng="Three";	break;
+		}
+		
+		System.out.println("[결과]"+kor+" = "+eng);
 		System.out.println("===============================================================");
 	}
 }
