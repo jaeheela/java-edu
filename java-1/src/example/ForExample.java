9dc2247f057aa03efc83b2c22d1b82b16e6d1ec1
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-19 17:47:59 +0900

    4Day

--- java-1/src/example/ForExample.java
+++ java-1/src/example/ForExample.java
@@ -38,7 +38,7 @@ public class ForExample {
 		//    43    44    45     46     47     48     49
 		//    50    51    52     53     54     55     56
 		//    57
-
+		int begin=36, end=57;
 
 		System.out.println("=========================================");	
 	}
