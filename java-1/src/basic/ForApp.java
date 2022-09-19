65faef2d24fbc08b9678057b30495dbd1ff5e77a
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-19 15:27:46 +0900

    4Day

---
+++ java-1/src/basic/ForApp.java
@@ -0,0 +1,40 @@
+package basic;
+
+//for : 명령에 대한 반복 횟수가 정해져 있는 경우 사용되는 반복문(Loop Statement) 
+//형식) for(초기식;조건식;증감식) { 명령; 명령; ... }
+// => 초기식 : 변수에 초기값을 저장하는 연산식, 증감식 : 변수값을 증가 또는 감소하는 연산식
+// => 초기식, 조건식, 증감식은 반드시 ;를 이용하여 구분 - ; 기호를 반드시 2번 사용
+// => 초기식 >> 조건식(참) - { } 안의 명령 실행 >> 증감식 >> 조건식(참) - { } 안의 명령 실행 >> 증감식 >> ...
+// => 조건식의 결과가 거짓(false)인 경우 for 구문 종료
+// => 주의)조건식의 결과가 항상 참(true)인 경우 무한반복 - 무한루프(Endless Loop)
+// => 초기식, 증감식은 , 기호 이용하여 나열 작성 가능
+// => 초기식, 조건식, 증감식 생략 가능 - 조건식이 생략된 경우 무조건 참으로 처리
+
+public class ForApp {
+	public static void main(String[] args) {
+		//"Java Programming"를 화면에 5번 출력
+		System.out.println("Java Programming");
+		System.out.println("Java Programming");
+		System.out.println("Java Programming");
+		System.out.println("Java Programming");
+		System.out.println("Java Programming");
+		System.out.println("==============================================================-");
+		//"Java Programming"를 화면에 5번 출력
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
