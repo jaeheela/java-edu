7dfc9d1c0b342a46e86c778535126dcf89e4aecf
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-22 14:56:02 +0900

    7Day

--- java-2/src/oop/ArraySortApp.java
+++ java-2/src/oop/ArraySortApp.java
@@ -1,8 +1,19 @@
 package oop;
 
+import java.util.Arrays;
+
+//OOP(Object Oriented Programming) : 객체 지향 프로그램 - 객체를 사용하여 프로그램 작성
+// => 현실 세계에 존재하는 대상을 모델링하여 클래스로 작성하고 클래스로 객체를 생성하여
+//    객체의 요소로 프로그램을 작성하는 방법
+// => 추상화, 캡슐화, 다형성, 상속성
+//객체 모델링(Object Modeling) : 현실 세계에 존재하는 대상을 속성과 행위로 구분하여 설계 
+// => 객체 모델링한 대상을 클래스(필드와 메소드)로 선언 - 추상화(Abstraction)
+// => 클래스 배포 가능 : 프로그램의 생산성이 증가되며 유지보수의 효율성 증가
+
 //배열 요소값을 오름차순 정렬되도록 저장하고 출력하는 프로그램 작성
 public class ArraySortApp {
 	public static void main(String[] args) {
+		/*
 		int[] array={30,50,10,40,20};
 		
 		System.out.print("정렬 전 >> ");
@@ -27,5 +38,31 @@ public class ArraySortApp {
 			System.out.print(num+" ");
 		}
 		System.out.println();
+		*/
+		
+		int[] array={30,50,10,40,20};
+
+		//Arrays 클래스 : 배열의 요소값을 처리하는 기능을 제공하는 클래스
+		//Arrays.toString(Object[] array) : 배열을 전달받아 배열의 모든 요소값들을 문자열로 
+		//변환하여 반환하는 메소드 
+		System.out.println("정렬 전 >> "+Arrays.toString(array));
+
+		//Arrays.sort(Object[] array) : 배열을 전달받아 배열의 요소값을 오름차순 정렬하여 저장하는 메소드
+		Arrays.sort(array);
+		
+		System.out.println("정렬 후 >> "+Arrays.toString(array));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
