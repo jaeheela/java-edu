7bb129a2e08016b93ca92a5ceae38b930494bd1a
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-22 12:14:13 +0900

    7Day

--- java-1/src/example/ArrayExample.java
+++ java-1/src/example/ArrayExample.java
@@ -2,16 +2,78 @@ package example;
 
 public class ArrayExample {
 	public static void main(String[] args) {
-		//사람들의 나이를 저장한 배열 생성		
+		//사람들의 나이를 저장한 배열 생성하여 참조변수에 저장		
 		int[] age={27,16,22,36,57,60,43,23,14,29,44,52,59,51,39,33,11};
 		
-		//배열에 저장된 모든 사람들이 나이 평균을 계산하여 출력하세요.
+		//배열에 저장된 모든 사람들의 나이 평균을 계산하여 출력하세요.
+		int tot=0;
+		for(int nai:age) {
+			tot+=nai;
+		}
 		
+		System.out.println("평균 나이 = "+(tot/age.length)+"살");
+		System.out.println("===============================================================");
 		//배열에 저장된 사람들의 나이를 연령별로 구분하여 인원수를 계산하여 출력하세요.
 		//ex) 10대 = 3명
 		//    20대 = 4명
 		//    ...
-		//    60대 = 2명
+		//    60대 = 1명
+		/*
+		int cnt10=0, cnt20=0, cnt30=0, cnt40=0, cnt50=0, cnt60=0;
+		
+		for(int nai:age) {
+			if(nai>=10 && nai<=19) cnt10++;
+			else if(nai>=20 && nai<=29) cnt20++;
+			else if(nai>=30 && nai<=39) cnt30++;
+			else if(nai>=40 && nai<=49) cnt40++;
+			else if(nai>=50 && nai<=59) cnt50++;
+			else if(nai>=60 && nai<=69) cnt60++;
+		}
+		
+		System.out.println("10대 = "+cnt10+"명");
+		System.out.println("20대 = "+cnt20+"명");
+		System.out.println("30대 = "+cnt30+"명");
+		System.out.println("40대 = "+cnt40+"명");
+		System.out.println("50대 = "+cnt50+"명");
+		System.out.println("60대 = "+cnt60+"명");
+		*/
+		
+		int[] cnt=new int[6];//연령별 인원수를 저장하기 위한 배열 생성
+		
+		for(int nai:age) {
+			/*
+			if(nai>=10 && nai<=19) cnt[0]++;
+			else if(nai>=20 && nai<=29) cnt[1]++;
+			else if(nai>=30 && nai<=39) cnt[2]++;
+			else if(nai>=40 && nai<=49) cnt[3]++;
+			else if(nai>=50 && nai<=59) cnt[4]++;
+			else if(nai>=60 && nai<=69) cnt[5]++;
+			*/
+			
+			/*
+			switch (nai/10) {
+			case 1: cnt[0]++; break;
+			case 2: cnt[1]++; break;
+			case 3: cnt[2]++; break;
+			case 4: cnt[3]++; break;
+			case 5: cnt[4]++; break;
+			case 6: cnt[5]++; break;
+			}
+			*/
+			
+			cnt[nai/10-1]++;
+		}
+		
+		for(int i=0;i<cnt.length;i++) {
+			System.out.println((i+1)*10+"대 = "+cnt[i]+"명");
+		}	
+		System.out.println("===============================================================");
 	}
 	
 }
+
+
+
+
+
+
