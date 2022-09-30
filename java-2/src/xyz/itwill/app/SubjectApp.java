cc75d19803830d3550a8e7ff8829c957989e00ee
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-09-30 11:00:52 +0900

    11Day

--- java-2/src/xyz/itwill/app/SubjectApp.java
+++ java-2/src/xyz/itwill/app/SubjectApp.java
@@ -1,5 +1,16 @@
 package xyz.itwill.app;
 
+//패키지(Package) : 같은 목적의 자료형(참조형)를 명확히 구분하여 그룹화 하기 위해 사용
+// => Java 자료형(참조형) : 클래스(Class), 인터페이스(Interface), 열거형(Enum)
+// => Java 자료형을 보다 쉽게 관리하기 위해 패키지 이용 
+// => 패키지의 이름은 도메인을 역방향으로 나열하고 그룹명을 지정하여 작성하는 것을 권장
+//ex) itwill.xyz : 도메인 >> xyz.itwill.board
+//도메인(Domain) : 인터넷에서 개인 또는 그룹이 사용하기 위한 네트워크 식별자
+
+//패키지에 작성된 소스파일의 첫번째 줄에는 사용 패키지를 반드시 설정
+//형식) package 패키지명;
+// => 소스파일에 작성된 Java 자료형이 어떤 패키지에 선언되어 있는지를 명확하게 표현
+
 //import : 다른 패키지의 자료형을 현재 자료형에서 명확하게 표현하기 위해 사용하는 키워드
 //형식)import 패키지명.자료형; 
 // => package 명령 하단에 작성
@@ -11,17 +22,6 @@ import xyz.itwill.subject.JavaSubject;
 //이미 import 처리된 자료형과 같은 이름의 자료형은 import 처리 불가능
 // => 자료형을 사용시 패키지를 같이 표현하여 접근 가능
 //import xyz.uniwill.subject.JavaSubject;
-
-//패키지(Package) : 같은 목적의 자료형(참조형)를 명확히 구분하여 그룹화 하기 위해 패키지 사용
-// => Java 자료형(참조형) : 클래스(Class), 인터페이스(Interface), 열거형(Enum)
-// => Java 자료형을 보다 쉽게 관리하기 위해 패키지 이용 
-// => 패키지의 이름은 도메인을 역방향으로 나열하고 그룹명을 지정하여 작성하는 것을 권장
-// ex) itwill.xyz : 도메인 >> xyz.itwill.board
-//도메인(Domain) : 인터넷에서 개인 또는 그룹이 사용하기 위한 네트워크 식별자
-
-//패키지에 작성된 소스파일의 첫번째 줄에는 사용 패키지를 반드시 설정
-//형식) package 패키지명;
-// => 소스파일에 작성된 Java 자료형이 어떤 패키지에 선언되어 있는지를 명확하게 표현
 
 public class SubjectApp {
 	public static void main(String[] args) {
