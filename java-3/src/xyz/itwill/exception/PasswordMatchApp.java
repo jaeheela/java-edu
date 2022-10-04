2909628a884cdd8d21d09afed5e2407638b8af0f
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-04 16:12:40 +0900

    13Day

--- java-3/src/xyz/itwill/exception/PasswordMatchApp.java
+++ java-3/src/xyz/itwill/exception/PasswordMatchApp.java
@@ -1,10 +1,12 @@
 package xyz.itwill.exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //키보드로 정수값을 입력받아 저장된 비밀번호와 비교하여 비교결과를 출력하는 프로그램 작성
 public class PasswordMatchApp {
 	public static void main(String[] args) {
+		/*
 		Scanner scanner=new Scanner(System.in);
 		
 		System.out.print("비밀번호 입력 >> ");
@@ -17,5 +19,29 @@ public class PasswordMatchApp {
 		}
 		
 		scanner.close();
+		*/
+		
+		Scanner scanner=new Scanner(System.in);
+
+		try {
+			System.out.print("비밀번호 입력 >> ");
+			int password=scanner.nextInt();//InputMismatchException 발생 가능
+			
+			if(password!=123456) {
+				//인위적으로 예외 객체를 생성하여 예외를 발생시키는 명령 
+				// => 프로그램의 흐름이 catch 블럭으로 이동하여 명령 실행
+				//형식)throw new 예외클래스(String message);
+				throw new PasswordMismatchException("[결과]입력된 비밀번호가 다릅니다.");
+			}
+			
+			//인위적 예외가 발생되지 않을 경우 실행되는 명령 
+			System.out.println("[결과]입력된 비밀번호가 맞습니다.");
+		} catch (InputMismatchException e) {
+			System.out.println("[에러]숫자만 입력 가능합니다.");
+		} catch (PasswordMismatchException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			scanner.close();
+		}
 	}
 }
---
+++ java-3/src/xyz/itwill/exception/PasswordMismatchException.java
@@ -0,0 +1,17 @@
+package xyz.itwill.exception;
+
+//프로그램 개발자가 직접 선언한 예외클래스
+// => 반드시 Exception 클래스를 상속받아 작성 
+public class PasswordMismatchException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public PasswordMismatchException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PasswordMismatchException(String message) {
+		//Exception 클래스에는 예외 메세지를 저장하기 위한 필드 선언
+		// => super 키워드를 사용하여 Exception 클래스의 필드에 초기값(예외 메세지) 저장
+		super(message);
+	}
+}
