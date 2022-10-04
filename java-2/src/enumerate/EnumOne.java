8c788352c23f8c86af24aeb1f5f16e675b331cbe
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-04 10:19:12 +0900

    12Day

--- java-2/src/enumerate/EnumOne.java
+++ java-2/src/enumerate/EnumOne.java
@@ -5,6 +5,6 @@ package enumerate;
 // => 열거형의 이름은 파스칼 표기법을 이용하여 작성하는 것을 권장
 public enum EnumOne {
 	//상수필드 선언 - public static final int 생략 가능
-	// => 열거형의 상수필드에는 0부터 1씩 증가되는 저장값이 기본값으로 자동 저장
+	// => 열거형의 상수필드에는 0부터 1씩 증가되는 값이 기본값으로 자동 저장
 	INSERT,UPDATE,DELETE,SELECT;
 }
