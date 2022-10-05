1f54369e31fed32b92c27fce8ae23b212b8eb99b
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-05 16:05:35 +0900

    14Day

--- java-3/src/xyz/itwill/lang/ConsoleCalculateApp.java
+++ java-3/src/xyz/itwill/lang/ConsoleCalculateApp.java
@@ -18,19 +18,52 @@ public class ConsoleCalculateApp {
 		
 		scanner.close();
 		
+		//연산식에서 검색할 연산자가 저장된 String 배열 선언
+		String[] operatorArray={"*","/","+","-"};
 		
+		//연산식에서 연산자를 검색하여 연산자의 시작위치(Index)를 반환받아 저장
+		int index=-1;
+		for(String str:operatorArray) {
+			//String.lastIndexOf(String str) : String 객체에 저장된 문자열에서 매개변수에
+			//저장된 문자열을 뒤쪽부터 검색하여 문자열의 시작위치값을 반환하는 메소드
+			index=operation.lastIndexOf(str);
+			
+			//연산식에서 연산자가 검색된 경우 반복문 종료  
+			if(index!=-1) break;
+		}
+		
+		//연산식에서 연산자가 없거나 연산자의 위치가 잘못된 경우 프로그램 종료
+		if(index<=0 || index>operation.length()-1) {
+			System.out.println("[에러]연산식을 잘못 입력하였습니다.");
+			System.exit(0);
+		}
+		
+		try {
+			//연산식에서 연산자와 피연산자를 분리하여 저장
+			// => 연산자 앞부분의 피연산자 분리하여 정수값으로 변환해 변수에 저장
+			int num1=Integer.parseInt(operation.substring(0, index));
+			// => 연산자를 분리하여 변수에 저장
+			String operator=operation.substring(index, index+1);
+			// => 연산자 뒷부분의 피연산자 분리하여 정수값으로 변환해 저장
+			int num2=Integer.parseInt(operation.substring(index+1));
+			
+			//연산자를 비교하여 피연산자의 연산 결과값을 저장
+			int result=0;
+			switch (operator) {
+			case "*": result=num1*num2;	break;
+			case "/": result=num1/num2;	break;
+			case "+": result=num1+num2;	break;
+			case "-": result=num1-num2;	break;
+			}
+			
+			//연산 결과값 출력
+			System.out.println("[결과]"+result);
+		} catch (NumberFormatException e) {
+			System.out.println("[에러]연산식에 숫자가 아닌 문자가 입력 되었습니다.");
+		} catch (ArithmeticException e) {
+			System.out.println("[에러]0으로 나눌 수 없습니다.");
+		} catch (Exception e) {
+			System.out.println("[에러]프로그램 실행에 예기치 못한 오류가 발생 하였습니다.");
+		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
