da66f91cf9a45e54f0128448153b48acc873fadf
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-05 11:30:32 +0900

    13Day

--- java-3/src/xyz/itwill/lang/SystemApp.java
+++ java-3/src/xyz/itwill/lang/SystemApp.java
@@ -2,14 +2,17 @@ package xyz.itwill.lang;
 
 import java.util.Scanner;
 
+//Java API(Application Programming Interface) : 프로그램 개발에 필요한 기능을 제공하기 위한 도구
+// => 라이브러리를 이용하여 배포된 클래스, 인터페이스, 열거형
+
 //java.lang 패키지 : Java 프로그램 개발에 필요한 기본 클래스를 제공하는 패키지
 // => java.lang 패키지의 클래스 또는 인터페이스는 import 처리하지 않아도 사용 가능
 
 //Object 클래스 : 모든 Java 클래스가 반드시 상속받는 부모클래스
 //System 클래스 : 표준 입출력 객체 및 시스템 관련 기능을 메소드로 제공하는 클래스
 //Class 클래스 : 메모리에 저장된 클래스를 저장하고 클래스 관련 기능을 메소드로 제공하는 클래스
-// => Class.forName(String className) : 문자열로 클래스를 전달받아 메모리에 저장하는 메소드 - Class 객체 반환
-// => Class.getName() : 메모리에 저장된 클래스의 이름을 반환하는 메소드
+// => Class.forName(String className) : 문자열로 클래스를 전달받아 메모리에 저장하는 메소드 - Class 객체(Clazz) 반환
+// => Class.getName() : Class 객체에 저장된 클래스의 이름을 반환하는 메소드
 //Math 클래스 : 수학 관련 기능을 메소드로 제공하는 클래스
 //String 클래스 : 문자열을 저장하고 문자열 관련 기능을 메소드로 재공하는 클래스
 //StringBuffer 클래스 : 문자열을 저장하고 문자열의 문자 관련 기능을 메소드로 재공하는 클래스
@@ -33,7 +36,7 @@ public class SystemApp {
 		}
 
 		//System.currentTimeMillis() : 시스템의 현재 타임스템프(TimeStamp)를 반환하는 메소드
-		//타임스템프(TimeStamp) : 날짜와 시간을 정수값으로 표현하기 위해 만들어진 값
+		//타임스템프(TimeStamp) : 날짜와 시간을 정수값으로 표현하기 위해 만들어진 시간값
 		// => 1970년 1월 1일을 기준으로 1/1000초당 1씩 증가된 long 타입의 정수값
 		long startTime=System.currentTimeMillis();
 		
