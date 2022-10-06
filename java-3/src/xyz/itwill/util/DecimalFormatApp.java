d12698c0c9e7cffc7728f3c2fd3cb1a4124459a0
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-06 17:35:10 +0900

    15Day

--- java-3/src/xyz/itwill/util/DecimalFormatApp.java
+++ java-3/src/xyz/itwill/util/DecimalFormatApp.java
@@ -9,7 +9,7 @@ public class DecimalFormatApp {
 		// => 패턴문자 : #(숫자 - 공백), 0(숫자 - 0), 콤마(,), 소숫점(.), 달러($)
 		DecimalFormat decimalFormat=new DecimalFormat("###,###,##0");
 		
-		int money=100000000;
+		int money=100_000_000;
 		System.out.println("금액 = "+money+"원");
 		
 		//DecimalFormat.format(long number) : 매개변수로 숫자값을 전달받아 DecimalFormat 
@@ -19,11 +19,11 @@ public class DecimalFormatApp {
 		decimalFormat.applyPattern("$##,###,##0.00");
 		System.out.println("금액 = "+decimalFormat.format(money));
 		
-		//DecimalFormat.getInstance() : 숫자값에 대한 기본 패턴정보가 저장된 DecimalFormat 객체를 반환하는 메소드
+		//DecimalFormat.getInstance() : 기본 패턴정보가 저장된 DecimalFormat 객체를 반환하는 메소드
 		System.out.println("금액 = "+DecimalFormat.getInstance().format(money)+"원");
 
-		//DecimalFormat.getInstance() : 숫자값에 대한 화폐단위가 포함된 기본 패턴정보가 저장된
-		//DecimalFormat 객체를 반환하는 메소드
+		//DecimalFormat.getInstance() : 기본 패턴정보가 저장된 DecimalFormat 객체를 반환하는 메소드
+		// => 변환되는 문자열 앞에 시스템에서 사용하는 화폐단위가 추가되어 변환
 		System.out.println("금액 = "+DecimalFormat.getCurrencyInstance().format(money));
 	}
 }
