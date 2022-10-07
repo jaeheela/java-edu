1d6619f14a95329540ebdf92001a2d338fb3c996
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-07 18:00:18 +0900

    16Day

--- java-3/src/xyz/itwill/util/StudentSetApp.java
+++ java-3/src/xyz/itwill/util/StudentSetApp.java
@@ -15,8 +15,9 @@ public class StudentSetApp {
 		// => Student 객체에 저장된 값은 같지만 객체는 다르므로 요소로 저장 가능
 		//hashCode() 메소드와 equals() 메소드를 오버라이드 선언하여 동일한 값이 저장된
 		//객체가 Set 객체의 요소로 저장하지 않도록 설정 가능
-		// => 매개변수로 전달받은 객체를 기존 요소의 HahsCode와 비교하고 같은 경우 필드값을
-		//비교하여 같으면 Set 객체에 추가되지 않도록 동작
+		// => 매개변수로 전달받은 객체를 hashCode() 메소드로 기존 요소의 HashCode와 비교하여 
+		//같은 경우 equals() 메소드로 필드값을 비교해 같으면 Set 객체에 추가되지 않도록 동작
+		// => HashCode가 다르거나 비교 필드값이 다르면 Set 객체에 요소가 추가되어 저장
 		studentSet.add(new Student(4000, "홍길동"));
 		
 		Iterator<Student> iterator=studentSet.iterator();
