f9bf46021c14ef72c18bf6e188e25b3fe7cfa0aa
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-07 12:46:38 +0900

    16Day

---
+++ java-3/src/xyz/itwill/util/SetApp.java
@@ -0,0 +1,13 @@
+package xyz.itwill.util;
+
+//자료구조 관련 클래스 - 데이타 수집 클래스(Collection Class)
+// => 다수의 객체를 효율적으로 저장하고 관리하기 위한 기능을 제공하는 클래스
+// => Set 인터페이스, List 인터페이스, Map 인터페이스를 중 하나를 상속받아 작성된 자식클래스
+
+//Set 인터페이스를 상속받은 자료구조 클래스 - HashSet, TreeSet 등
+// => 객체의 저장 순서가 없으며 동일한 객체 중복 저장 불가능
+public class SetApp {
+	public static void main(String[] args) {
+		
+	}
+}
