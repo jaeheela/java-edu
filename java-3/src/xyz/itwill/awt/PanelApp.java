b70a318107aa8173c76cef647560793c0b5fdb50
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-12 10:55:42 +0900

    17Day

--- java-3/src/xyz/itwill/awt/PanelApp.java
+++ java-3/src/xyz/itwill/awt/PanelApp.java
@@ -2,7 +2,7 @@ package xyz.itwill.awt;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Color;
+import java.awt.Color; 
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Panel;
