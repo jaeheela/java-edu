c76c88627f39250f367c4bcdb206172071dc63dd
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-10-13 15:15:19 +0900

    19Day

--- java-3/src/xyz/itwill/io/ByteStreamApp.java
+++ java-3/src/xyz/itwill/io/ByteStreamApp.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 //원시데이타 기반 스트림(Byte Stream) : 가공되지 않은 원시데이타를 전달하기 위한 입출력 스트림
 // => 값을 1Byte 단위로 입력 또는 출력하기 위한 스트림
-// => InputStream 클래스와 OutputStream 클래스를 최상위 클래스로 하는 자식클래스을 이용하여 생성
+// => InputStream 클래스와 OutputStream 클래스를 최상위 클래스로 하는 자식클래스를 이용하여 생성
 
 //키보드로 원시데이타를 입력받아 모니터에 전달하여 출력하는 프로그램
 // => EOF(End Of File : 입력종료 - Ctrl+Z) 신호를 입력하면 프로그램 종료
---
+++ java-3/src/xyz/itwill/io/CharacterStreamApp.java
@@ -0,0 +1,13 @@
+package xyz.itwill.io;
+
+//문자데이타 기반 스트림(Character Stream) : 원시데이타를 가공처리한 문자데이타를 전달하기 위한 입출력 스트림
+// => 값을 2Byte 단위로 입력 또는 출력하기 위한 스트림
+// => Reader 클래스와 Writer 클래스를 최상위 클래스로 하는 자식클래스를 이용하여 생성
+
+//키보드로 문자데이타를 입력받아 모니터에 전달하여 출력하는 프로그램
+// => EOF(End Of File : 입력종료 - Ctrl+Z) 신호를 입력하면 프로그램 종료
+public class CharacterStreamApp {
+	public static void main(String[] args) {
+		
+	}
+}
