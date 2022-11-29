42bb0a0c02eda0b92b29f8b183a2c4e33a26a74b
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-11-29 12:01:56 +0900

    51Day

--- servlet/src/main/java/xyz/itwill/servlet/HelloServlet.java
+++ servlet/src/main/java/xyz/itwill/servlet/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//Servlet : Java Web Server Program - Server + let 합성어로 웹서버에서 실행되는 간단한 프로그램
+//Servlet : Server + let 합성어로 웹서버에서 실행되는 간단한 프로그램 - Java Web Server Program
 // => 웹프로그램은 클라이언트 요청에 의해 WAS(Web Application Server)로 실행되며 실행결과를
 //웹문서를 동적으로 생성하여 응답 - Servlet 프로그램은 WAS에 의해 관리(생성,사용,소멸)  
 //WAS(Web Application Server) : Web Server(요청과 응답) + Web Container(서블릿 관리)
@@ -20,14 +20,15 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	//2.doGet() 메소드 또는 doPost() 메소드를 오버라이드 선언
-	// => 클라이언트 요청에 대한 처리와 처리결과를 동적으로 웹문서로 생성하여 응답하기 위한 메소드
-	// => WAS(Web Application Server) 프로그램에 의해 자동 호출되는 메소드
+	// => 클라이언트 요청에 대한 처리와 처리결과를 이용하여 동적으로 웹문서를 생성하여 응답
+	//하기 위한 메소드 - 요청 처리 메소드 : WAS 프로그램에 의해 자동 호출
 	// => doGet() : 클라이언트가 해당 웹프로그램을 GET 방식으로 요청한 경우 호출되는 메소드
 	// => doPost() : 클라이언트가 해당 웹프로그램을 POST 방식으로 요청한 경우 호출되는 메소드
 	//doGet() 메소드 또는 doPost() 메소드 대신 service() 메소드 오버라이드 선언 가능
 	// => service() : 클라이언트가 해당 웹프로그램을 모든 방식으로 요청한 경우 호출되는 메소드
-	//WAS 프로그램에 의해 메소드가 호출될 때 HttpServletRequest 객체와 HttpServletResponse 객체가
-	//매개변수에 저장되어 제공
+	// => service() 메소드가 doGet() 메소드 또는 doPost() 메소드보다 호출 우선순위가 높게 설정
+	//WAS 프로그램에 의해 요청 처리 메소드가 호출될 때 HttpServletRequest 객체(request)와 
+	//HttpServletResponse 객체(response)가 매개변수에 저장되어 제공
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//HttpServletRequest : 리퀘스트 메세지(요청 관련 정보)를 표현한 객체 - 입력 
@@ -47,7 +48,6 @@ public class HelloServlet extends HttpServlet {
 		//있는 출력스트림(PrintWriter 객체)를 반환하는 메소드
 		// => PlanText, HTML, XML, JSON 파일등과 같은 문서파일을 생성하여 클라이언트에게 응답하기 위한 출력스트림
 		PrintWriter out=response.getWriter();
-		
 		//5.요청에 대한 처리와 처리에 대한 결과를 출력스트림을 사용하여 응답파일 생성
 		out.println("<!DOCTYPE html>");//출력스트림으로 문자열(HTML 태그)을 전달하여 문서파일 작성
 		out.println("<html>");
@@ -63,25 +63,10 @@ public class HelloServlet extends HttpServlet {
 		out.println("</html>");
 		
 		//6.서블릿 클래스를 서블릿(웹프로그램)으로 실행되도록 설정
-		// => [web.xml] 파일에서 서블릿 클래스를 서블릿으로 등록하여 서블릿을 요청할 수 있는
-		//URL 주소를 매핑 설정
+		// => [web.xml] 파일에서 서블릿 클래스를 서블릿(웹프로그램)으로 등록하여 서블릿을 
+		//요청할 수 있는 URL 주소를 매핑 설정
 		//클라이언트가 매핑된 URL 주소로 서블릿을 요청하면 WAS 프로그램은 서블릿 클래스를
 		//객체로 생성하여 요청 처리 메소드를 호출하여 요청에 대한 처리와 응답결과 생성
 		// => 서블릿 클래스가 이미 객체로 생성된 경우에는 요청 처리 메소드만 호출하여 실행
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
