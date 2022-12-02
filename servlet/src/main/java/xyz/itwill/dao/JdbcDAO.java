ce57f7260415e8fda8fdcad1a6bf602ca7e13ca3
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-12-02 15:57:26 +0900

    55Day

--- servlet/src/main/java/xyz/itwill/dao/JdbcDAO.java
+++ servlet/src/main/java/xyz/itwill/dao/JdbcDAO.java
@@ -1,5 +1,10 @@
 package xyz.itwill.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -16,6 +21,37 @@ public abstract class JdbcDAO {
 		try {
 			dataSource=(DataSource)new InitialContext().lookup("java:comp/env/jdbc/oracle");
 		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+	
+	public void close(Connection con) {
+		try {
+			if(con!=null) con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close(Connection con, PreparedStatement pstmt) {
+		try {
+			if(pstmt!=null) pstmt.close();
+			if(con!=null) con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		try {
+			if(rs!=null) rs.close();
+			if(pstmt!=null) pstmt.close();
+			if(con!=null) con.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
