9708f4292b27a95b4e77d0e98e92351d1345376e
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-12-02 13:21:09 +0900

    55Day

---
+++ servlet/src/main/java/xyz/itwill/dao/JdbcDAO.java
@@ -0,0 +1,30 @@
+package xyz.itwill.dao;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+//JDBC 기능을 제공하는 DAO 클래스가 상속받기 위해 작성된 부모클래스
+// => 객체 생성이 목적이 아닌 상속을 목적으로 작성된 클래스 - 추상클래스로 작성하는 것을 권장
+// => WAS 프로그램에 등록된 자원을 얻어와 DataSource 객체를 반환받아 저장 - 정적영역을 이용하여 한번만 실행
+// => DataSource 객체로부터 Connection 객체를 제공받아 반환하는 메소드
+// => 매개변수로 JDBC 관련 객체를 제공받아 제거하는 메소드
+public abstract class JdbcDAO {
+	private static DataSource dataSource;
+	
+	static {
+		try {
+			dataSource=(DataSource)new InitialContext().lookup("java:comp/env/jdbc/oracle");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
+
---
+++ servlet/src/main/java/xyz/itwill/dao/PhonebookDAO.java
@@ -0,0 +1,56 @@
+package xyz.itwill.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.itwill.dto.PhonebookDTO;
+
+//DAO(Data Access Object) 클래스 : 테이블에 행 삽입,변경,삭제,검색 명령(SQL)을 전달하여  
+//실행하고 처리 결과를 Java 객체(값)으로 반환하는 기능을 제공하는 클래스
+// => 싱글톤 클래스(프로그램에 객체를 하나만 생성하여 제공하는 클래스)로 작성하는 것을 권장
+
+//PHONEBOOK 테이블에 회원정보를 삽입,변경,삭제,검색하는 기능을 제공하는 클래스
+public class PhonebookDAO {
+	private static PhonebookDAO _dao;
+	
+	private PhonebookDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static {
+		_dao=new PhonebookDAO();
+	}
+	
+	public static PhonebookDAO getDAO() {
+		return _dao;
+	}
+	
+	//PHONEBOOK 테이블에 저장된 모든 회원정보를 검색하여 반환하는 메소드
+	public List<PhonebookDTO> selectPhonebookList() {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<PhonebookDTO> phonebookList=new ArrayList<PhonebookDTO>();
+		try {
+			
+		} 
+		return phonebookList;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
