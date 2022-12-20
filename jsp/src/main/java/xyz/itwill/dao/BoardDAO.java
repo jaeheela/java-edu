d768e730ae1770a26929f80c8a724b1ffc2f9fd9
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-12-20 12:29:26 +0900

    64Day

---
+++ jsp/src/main/java/xyz/itwill/dao/BoardDAO.java
@@ -0,0 +1,19 @@
+package xyz.itwill.dao;
+
+public class BoardDAO extends JdbcDAO {
+	private static BoardDAO _dao;
+	
+	private BoardDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static {
+		_dao=new BoardDAO();
+	}
+	
+	public static BoardDAO getDAO() {
+		return _dao;
+	}
+	
+	
+}
