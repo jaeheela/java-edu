7c1dc867eceda095d520183d30b3778f083813b4
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2022-12-20 15:34:38 +0900

    64Day

--- jsp/src/main/java/xyz/itwill/dao/BoardDAO.java
+++ jsp/src/main/java/xyz/itwill/dao/BoardDAO.java
@@ -1,5 +1,14 @@
 package xyz.itwill.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.itwill.dto.BoardDTO;
+
 public class BoardDAO extends JdbcDAO {
 	private static BoardDAO _dao;
 	
@@ -15,5 +24,86 @@ public class BoardDAO extends JdbcDAO {
 		return _dao;
 	}
 	
+	//BOARD 테이블에 저장된 전체 게시글의 갯수를 검색하여 반환하는 메소드
+	public int selectBoardCount() {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		int count=0;
+		try {
+			con=getConnection();
+			
+			String sql="select count(*) from board";
+			pstmt=con.prepareStatement(sql);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				count=rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectBoardCount() 메서드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return count;
+	}
 	
+	//요청 페이지에 대한 시작 게시글의 행번호와 종료 게시글의 행번호를 전달받아 BAORD 테이블에
+	//저장된 게시글에서 해당 범위의 게시글만을 검색하여 반환하는 메소드
+	public List<BoardDTO> selectBoardList(int startRow, int endRow) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<BoardDTO> boardList=new ArrayList<>();
+		try {
+			con=getConnection();
+			
+			String sql="select * from (select rownum rn,temp.* from (select num,member.id"
+				+ ",name,subject,reg_date,readcount,ref,re_step,re_level,content,ip"
+				+ ",board.status from board join member on board.id=member.id"
+				+ " order by ref desc, re_step) temp) where rn between ? and ?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
+			
+			rs=pstmt.executeQuery();
+					
+			while(rs.next()) {
+				BoardDTO board=new BoardDTO();
+				board.setNum(rs.getInt("num"));
+				board.setId(rs.getString("id"));
+				board.setWriter(rs.getString("name"));
+				board.setSubject(rs.getString("subject"));
+				board.setRegDate(rs.getString("reg_date"));
+				board.setReadcount(rs.getInt("readcount"));
+				board.setRef(rs.getInt("ref"));
+				board.setReStep(rs.getInt("re_step"));
+				board.setReLevel(rs.getInt("re_level"));
+				board.setContent(rs.getString("content"));
+				board.setIp(rs.getString("ip"));
+				board.setStatus(rs.getInt("status"));
+				boardList.add(board);
+			}
+		} catch (SQLException e) {
+			System.out.println("[에러]selectBoardList() 메서드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return boardList;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- jsp/src/main/webapp/board/board_list.jsp
+++ jsp/src/main/webapp/board/board_list.jsp
@@ -1,3 +1,61 @@
-﻿<%@ page language="java" contentType="text/html; charset=UTF-8"
+﻿<%@page import="xyz.itwill.dao.BoardDAO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%-- BOARD 테이블에 저장된 게시글을 검색하여 게시글 목록을 클라이언트에게 전달하는 JSP 문서 --%>
+<%-- => 게시글 목록을 페이지로 구분 검색하여 응답 처리 - 페이징 처리 --%>    
+<%
+	//페이징 처리 관련 전달값(요청 페이지 번호)를 반환받아 저장
+	// => 요청 페이지 번호에 대한 전달값이 없는 경우 첫번째 페이지의 게시글 목록을 검색하여 응답
+	int pageNum=1;
+	if(request.getParameter("pageNum")!=null) {//전달값이 있는 경우
+		pageNum=Integer.parseInt(request.getParameter("pageNum"));
+	}
+	
+	//하나의 페이지에 검색되어 출력될 게시글의 갯수 설정 - 전달값을 반환받아 저장 가능
+	int pageSize=10;
+	
+	//BOARD 테이블에 저장된 전체 게시글의 갯수를 검색하여 반환하는 DAO 클래스의 메소드 호출
+	// => 검색 기능 미구현시 호출하는 메소드
+	int totalBoard=BoardDAO.getDAO().selectBoardCount();
+	            
+	//전체 페이지의 갯수를 계산하여 저장
+	//int totalPage=totalBoard/pageSize+totalBoard%pageSize==0?0:1;
+	int totalPage=(int)Math.ceil((double)totalBoard/pageSize);
+	  
+	//요청 페이지 번호에 대한 검증
+	if(pageNum<=0 || pageNum>totalPage) {//비정상적인 요청 페이지 번호인 경우
+		pageNum=1;//첫번째 페이지의 게시글 목록이 검색되어 응답되도록 요청 페이지 번호 변경
+	}
+	
+	//요청 페이지 번호에 대한 시작 게시글의 행번호를 계산하여 저장
+	//ex) 1Page : 1, 2Page : 11, 3Page : 21, 4Page : 31, ...
+	int startRow=(pageNum-1)*pageSize+1;
+                     
+	//요청 페이지 번호에 대한 종료 게시글의 행번호를 계산하여 저장
+	//ex) 1Page : 10, 2Page : 20, 3Page : 30, 4Page : 40, ...
+	int endRow=pageNum*pageSize;
+	
+	//마지막 페이지에 대한 종료 게시글의 행번호를 검색 게시글의 갯수로 변경
+	if(endRow>totalBoard) {
+		endRow=totalBoard;
+	}
+	
+	//요청 페이지에 대한 시작 게시글의 행번호와 종료 게시글의 행번호를 전달받아 BAORD 테이블에
+	//저장된 게시글에서 해당 범위의 게시글만을 검색하여 반환하는 DAO 클래스의 메소드 호출
+	// => 검색 기능 미구현시 호출하는 메소드
+
+	
+%>                      
 <h1>게시판 목록</h1>
+
+
+
+
+
+
+
+
+
+
+
+
