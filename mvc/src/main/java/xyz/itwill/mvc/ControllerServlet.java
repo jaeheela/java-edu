d4fa1ad13cdf1f84da799d2f961959dad530597f
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-01-17 17:51:49 +0900

    82Day

--- mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
+++ mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
@@ -65,6 +65,8 @@ public class ControllerServlet extends HttpServlet {
 			action=new LoginFormModel();
 		} else if(command.equals("/login.do")) {
 			action=new LoginModel();
+		} else if(command.equals("/logout.do")) {
+			action=new LogoutModel();
 		} else if(command.equals("/error.do")) {
 			action=new ErrorModel();
 		} else {//요청에 대한 모델 클래스가 없는 경우
