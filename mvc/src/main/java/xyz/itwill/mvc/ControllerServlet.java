df50dd505967ec2a6cdf407343b53c3966e443e4
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-01-18 12:40:34 +0900

    83Day

--- mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
+++ mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
@@ -70,6 +70,8 @@ public class ControllerServlet extends HttpServlet {
 			action=new WriteFormModel();
 		} else if(command.equals("/write.do")) {
 			action=new WriteModel();
+		} else if(command.equals("/list.do")) {
+			action=new ListModel();
 		} else if(command.equals("/error.do")) {
 			action=new ErrorModel();
 		} else {//요청에 대한 모델 클래스가 없는 경우
