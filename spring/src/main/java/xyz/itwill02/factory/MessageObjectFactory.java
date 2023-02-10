2b387f3862d7f3affaa7a4f60c5e381f54d20b84
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-10 16:29:15 +0900

    97Day

--- spring/src/main/java/xyz/itwill02/factory/MessageObjectFactory.java
+++ spring/src/main/java/xyz/itwill02/factory/MessageObjectFactory.java
@@ -19,6 +19,7 @@ public class MessageObjectFactory {
 	
 	//Factory 클래스에 의해 제공될 객체를 구분하기 위한 상수(Constant)
 	public static final int HELLO_MSG=1;
+	public static final int HI_MSG=2;
 	
 	//매개변수로 전달받은 값을 비교하여 프로그램 개발에 필요한 객체를 생성하여 반환하는 메소드
 	public MessageObject getMessageObject(int messageObject) {
@@ -27,19 +28,10 @@ public class MessageObjectFactory {
 		case HELLO_MSG:
 			object=new HelloMessageObject();
 			break;
+		case HI_MSG:
+			object=new HiMessageObject();
+			break;	
 		}
 		return object;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- spring/src/main/java/xyz/itwill02/factory/MessagePrint.java
+++ spring/src/main/java/xyz/itwill02/factory/MessagePrint.java
@@ -7,7 +7,8 @@ public class MessagePrint {
 		
 		//프로그램 작성에 필요한 객체를 Factory 클래스로부터 제공받아 메소드 호출 
 		// => IoC(Inversion of Control) : 객체간의 결합도를 낮춰 유지보수의 효율성 증가
-		MessageObject object=MessageObjectFactory.getFactory().getMessageObject(1);
+		//MessageObject object=MessageObjectFactory.getFactory().getMessageObject(1);
+		MessageObject object=MessageObjectFactory.getFactory().getMessageObject(2);
 		
 		//인터페이스로 생성된 참조변수의 추상메소드를 호출한 경우 참조변수에 저장된 자식 클래스
 		//객체의 오버라이드 메소드 호출 - 묵시적 객체 형변환 : 오버라이드에 의한 다형성
