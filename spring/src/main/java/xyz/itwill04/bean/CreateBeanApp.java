6e6384c6586c966e2c53c23ea59dc54164837998
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-13 11:32:17 +0900

    98Day

---
+++ spring/src/main/java/xyz/itwill04/bean/CreateBeanApp.java
@@ -0,0 +1,11 @@
+package xyz.itwill04.bean;
+
+//Spring Framework에서는 BeanFactory 객체 또는 ApplicationContext 객체로 스프링 컨테이너
+//(Spring Container) 기능 제공
+// => 스프링 컨테이너는 환경설정파일(Spring Bean Configuration File - XML)을 제공받아 클래스의
+//객체(Spring Bean)를 관리 
+public class CreateBeanApp {
+	public static void main(String[] args) {
+		
+	}
+}
