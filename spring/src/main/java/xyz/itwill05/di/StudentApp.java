6e9449d40f6d194064d6e1563fba887b0d7e5270
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-14 09:47:52 +0900

    98Day

--- spring/src/main/java/xyz/itwill05/di/StudentApp.java
+++ spring/src/main/java/xyz/itwill05/di/StudentApp.java
@@ -8,7 +8,12 @@ public class StudentApp {
 		System.out.println("================== Spring Container 초기화 전 ==================");
 		ApplicationContext context=new ClassPathXmlApplicationContext("05-1_di.xml");
 		System.out.println("================== Spring Container 초기화 후 ==================");
-
+		Student student1=context.getBean("student1",Student.class);
+		//참조변수 출력시 Student 클래스의 toString() 메소드 자동 호출 - 객체의 필드값 확인
+		System.out.println(student1);
+		System.out.println("================================================================");
+		Student student2=context.getBean("student2",Student.class);
+		System.out.println(student2);
 		System.out.println("================================================================");
 		((ClassPathXmlApplicationContext)context).close();
 	}
