536ed64dc4ccad80d6090341a9c2d01c69c21e51
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-14 13:22:19 +0900

    99Day

--- spring/src/main/java/xyz/itwill05/di/StudentApp.java
+++ spring/src/main/java/xyz/itwill05/di/StudentApp.java
@@ -27,6 +27,35 @@ public class StudentApp {
 		Student student6=context.getBean("student6",Student.class);
 		System.out.println(student6);
 		System.out.println("================================================================");
+		//프로그램 실행에 필요한 데이타 처리 기능은 Service 클래스의 메소드를 호출하여 사용
+		// => 스프링 컨테이너에게 Service 클래스의 객체를 제공받아 메소드 호출
+		//StudentServiceImpl service=context.getBean("studentServiceImpl", StudentServiceImpl.class);
+		
+		//클래스로 참조변수를 생성하여 객체를 반환받아 저장하는 것보다는 인터페이스로 참조변수를
+		//생성하여 객체를 저장하는 것이 유지보수의 효율성 증가
+		// => 인터페이스로 반환받기 위한 객체의 형변환 가능
+		StudentService service=context.getBean("studentServiceImpl", StudentService.class);
+		
+		service.addStudent(student1);
+		service.modifyStudent(student1);
+		service.removeStudent(1000);
+		service.getStudent(100);
+		service.getStudentList();
+		System.out.println("================================================================");
 		((ClassPathXmlApplicationContext)context).close();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spring/src/main/java/xyz/itwill05/di/StudentServiceImpl.java
+++ spring/src/main/java/xyz/itwill05/di/StudentServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 //Service 클래스 : 프로그램 실행에 필요한 데이타 처리 기능을 모듈화하여 제공하는 클래스 - 컴퍼넌트
 // => Service 클래스의 메소드는 DAO 클래스의 메소드를 호출하여 작성 - 모듈화
 // => DAO 클래스는 Service 클래스와 포함관계(의존관계)로 설정되도록 작성
-//=> Service 클래스가 변경돼도 Service 클래스와 관계가 있는 클래스(모델 클래스)에 영향을 최소화
+// => Service 클래스가 변경돼도 Service 클래스와 관계가 있는 클래스(모델 클래스)에 영향을 최소화
 //하기 위해 반드시 인터페이스 상속받아 작성 - 결합도를 낮춰 유지보수의 효율성 증가
 public class StudentServiceImpl implements StudentService {
 	//StudentJdbcDAO 객체를 저장하기 위한 필드
@@ -46,27 +46,31 @@ public class StudentServiceImpl implements StudentService {
 	@Override
 	public void addStudent(Student student) {
 		System.out.println("*** StudentServiceImpl 클래스 addStudent(Student student) 메소드 호출 ***");
+		studentDAO.insertStudent(student);
 	}
 
 	@Override
 	public void modifyStudent(Student student) {
 		System.out.println("*** StudentServiceImpl 클래스 modifyStudent(Student student) 메소드 호출 ***");
+		studentDAO.updateStudent(student);
 	}
 
 	@Override
 	public void removeStudent(int num) {
 		System.out.println("*** StudentServiceImpl 클래스 removeStudent(int num) 메소드 호출 ***");
+		studentDAO.deleteStudent(num);
 	}
 
 	@Override
 	public Student getStudent(int num) {
 		System.out.println("*** StudentServiceImpl 클래스 getStudent(int num) 메소드 호출 ***");
-		return null;
+		return studentDAO.selectStudent(num);
+		
 	}
 
 	@Override
 	public List<Student> getStudentList() {
 		System.out.println("*** StudentServiceImpl 클래스 getStudentList() 메소드 호출 ***");
-		return null;
+		return studentDAO.selectStudentList();
 	}
 }
