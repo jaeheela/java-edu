6ae04dd43b466c12f7223080336e78f1c3fbb2ec
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-15 12:07:20 +0900

    99Day

--- spring/src/main/java/xyz/itwill05/di/AutoWireApp.java
+++ spring/src/main/java/xyz/itwill05/di/AutoWireApp.java
@@ -8,7 +8,8 @@ public class AutoWireApp {
 		System.out.println("================== Spring Container 초기화 전 ==================");
 		ApplicationContext context=new ClassPathXmlApplicationContext("05-3_autowire.xml");
 		System.out.println("================== Spring Container 초기화 후 ==================");
-
+		StudentService service=context.getBean("studentService",StudentService.class);
+		service.addStudent(null);
 		System.out.println("================================================================");
 		((ClassPathXmlApplicationContext)context).close();		
 	}
