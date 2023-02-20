003e53ff070a77cd3b0a58f44b67eb727ac0d7a9
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-20 11:40:58 +0900

    102Day

---
+++ spring/src/main/java/xyz/itwill07/aop/EmailSendApp.java
@@ -0,0 +1,16 @@
+package xyz.itwill07.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmailSendApp {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("07-4_email.xml");
+		EmailSendBean bean=context.getBean("emailSendBean", EmailSendBean.class);
+		System.out.println("================================================================");
+		bean.sendEmail("ocj1778@hanmail.net", "메일 전송 테스트"
+				, "JavaMail 기능을 사용하여 전달된 이메일입니다.");
+		System.out.println("================================================================");
+		((ClassPathXmlApplicationContext)context).close();	
+	}
+}
