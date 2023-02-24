ab377e0ffe3329a72a0e32813dae7037e345c289
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-24 13:18:29 +0900

    106Day

---
+++ spring/src/main/java/xyz/itwill10/controller/TilesController.java
@@ -0,0 +1,9 @@
+package xyz.itwill10.controller;
+
+//TilesView : 요청 처리 메소드의 반환값(ViewName)을 제공받아 다수의 JSP 문서를 합쳐 하나의
+//템플릿 페이지의 JSP 문서로 응답하기 위한 기능을 제공하는 뷰프로그램 - ViewResolver
+//1.TilesView 관련 라이브러리(tiles-extras)를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
+
+public class TilesController {
+
+}
