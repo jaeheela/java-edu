d824abfe189e4b5619a82d3680f5a4d8a65631e9
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-24 16:29:49 +0900

    106Day

--- spring/src/main/java/xyz/itwill10/controller/TilesController.java
+++ spring/src/main/java/xyz/itwill10/controller/TilesController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //1.TilesView 관련 라이브러리(tiles-extras)를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
 //2.요청 처리 메소드의 반환값(ViewName)을 제공받아 응답할 템플릿 페이지 설정
 // => TilesView 환경설정파일(XML)을 작성하여 설정 - /WEB-INF/spring/appServlet/tiles.xml
+//3.Front Controller가 요청 처리 메소드의 반환값(ViewName)을 제공받아 TilesView를 이용하여 
+//응답 처리되도록 Spring Bean Configuration File 설정 - servlet-context.xml
+// => 기존의 ViewName을 JSP 문서로 변환한 InternalResourceViewResolver보다 먼저 실행되도록 설정
 
 @Controller
 public class TilesController {
