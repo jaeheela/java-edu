f9b05c0aa1912fae04ed74d17cc72d59109b785a
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-02-27 15:04:37 +0900

    107Day

--- spring/src/main/java/xyz/itwill10/controller/TilesController.java
+++ spring/src/main/java/xyz/itwill10/controller/TilesController.java
@@ -4,18 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //TilesView : 요청 처리 메소드의 반환값(ViewName)을 제공받아 다수의 JSP 문서를 합쳐 하나의
-//템플릿 페이지의 JSP 문서로 응답하기 위한 기능을 제공하는 뷰프로그램 - ViewResolver
+//JSP 문서(템플릿 페이지)로 응답하기 위한 기능을 제공하는 뷰프로그램 - ViewResolver
 //1.TilesView 관련 라이브러리(tiles-extras)를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
 //2.요청 처리 메소드의 반환값(ViewName)을 제공받아 응답할 템플릿 페이지 설정
 // => TilesView 환경설정파일(XML)을 작성하여 설정 - /WEB-INF/spring/appServlet/tiles.xml
 //3.Front Controller가 요청 처리 메소드의 반환값(ViewName)을 제공받아 TilesView를 이용하여 
 //응답 처리되도록 Spring Bean Configuration File 설정 - servlet-context.xml
-// => 기존의 ViewName을 JSP 문서로 변환한 InternalResourceViewResolver보다 먼저 실행되도록 설정
+// => 기존의 ViewName을 JSP 문서로 변환하여 응답한 InternalResourceViewResolver보다 먼저 실행되도록 설정
 
 @Controller
 public class TilesController {
 	@RequestMapping("/")
 	public String tiles() {
 		return "main";
+	}
+	
+	@RequestMapping("/tiles1")
+	public String tilesOne() {
+		return "tiles/view";
+	}
+	
+	@RequestMapping("/tiles2")
+	public String tilesTwo() {
+		return "tiles/sub/view";
+	}
+	
+	@RequestMapping("/mgr")
+	public String tilesAdmin() {
+		return "admin";
+	}
+	
+	@RequestMapping("/mgr_member")
+	public String tilesMember() {
+		return "admin/member";
 	}
 }
