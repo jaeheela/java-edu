1f910ded2a7b22a15e80f8294a7540ab4f5c7f6b
ocj1778 <68932217+ocj1778@users.noreply.github.com>
2023-03-07 16:28:47 +0900

    112Day

--- spring/src/main/java/xyz/itwill10/controller/FileController.java
+++ spring/src/main/java/xyz/itwill10/controller/FileController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+//파일 업로드 처리를 위한 환경설정 방법
+//1.commons-fileupload 라이브러리를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
+//2.Spring Bean Configuration File(servlet-context.xml)에 파일 업로드 기능을 제공하는 
+//클래스를 Spring Bean으로 등록
+//3.MultipartHttpServletRequest 객체를 사용하여 [multipart/form-data] 형태로 전달된 값 또는 파일 처리
+
 @Controller
 public class FileController {
 	@RequestMapping(value = "/upload", method = RequestMethod.GET)
@@ -24,24 +30,24 @@ public class FileController {
 	public String upload(MultipartHttpServletRequest request) throws IOException {
 		//MultipartHttpServletRequest.getParameter(String name) : [multipart/form-data] 
 		//형식으로 전달된 값을 문자열(String 객체)로 반환하는 메소드
-		String username=request.getParameter("username");
+		String uploaderName=request.getParameter("uploaderName");
 		
 		//MultipartHttpServletRequest.getFile(String name) : [multipart/form-data] 형식으로 
 		//전달된 파일을 MultipartFile 객체로 반환하는 메소드
 		//MultipartFile 객체 : 사용자로부터 입력되어 전달된 파일정보를 저장하기 위한 객체
-		MultipartFile filename=request.getFile("filename");
+		MultipartFile uploadFile=request.getFile("uploadFile");
 	
 		//전달받은 파일에 대한 검증 작업
 		//MultipartFile.isEmpty() : MultipartFile 객체에 파일정보가 없는 경우 [false]를 반환하고 
 		//파일정보가 있는 경우 [true]를 반환하는 메소드
-		if(filename.isEmpty()) {
+		if(uploadFile.isEmpty()) {
 			return "file/upload_fail";
 		}
 		
 		//MultipartFile.getContentType() :  MultipartFile 객체에 저장된 파일의 형식(MimeType)를 반환하는 메소드 
-		System.out.println("파일 형식 = "+filename.getContentType());
+		System.out.println("파일 형식 = "+uploadFile.getContentType());
 		//MultipartFile.getBytes() : MultipartFile 객체에 저장된 파일정보를 byte 배열로 반환하는 메소드
-		System.out.println("파일 크기 = "+filename.getBytes().length);
+		System.out.println("파일 크기 = "+uploadFile.getBytes().length);
 	
 		//전달파일을 저장하기 위한 서버 디렉토리의 시스템 경로를 반환받아 저장
 		String uploadDirectory=request.getServletContext().getRealPath("/resources/images/upload");
@@ -50,14 +56,14 @@ public class FileController {
 		//전달파일을 서버 디렉토리에 저장하기 위한 File 객체 생성
 		//File 객체 : 시스템에 존재하는 파일정보를 저장하기 위한 객체
 		//MultipartFile.getOriginalFilename() : 전달파일의 파일명을 반환하는 메소드
-		File file=new File(uploadDirectory, filename.getOriginalFilename());
+		File file=new File(uploadDirectory, uploadFile.getOriginalFilename());
 		
 		//MultipartFile.transferTo(File file) : MultipartFile 객체에 저장된 파일정보를
 		//File 객체에 저장된 파일정보로 전달하여 저장하는 메소드 - 업로드 처리 메소드
-		filename.transferTo(file);
+		uploadFile.transferTo(file);
 		
-		request.setAttribute("username", username);
-		request.setAttribute("filename", filename.getOriginalFilename());
+		request.setAttribute("uploaderName", uploaderName);
+		request.setAttribute("uploadFilename", uploadFile.getOriginalFilename());
 		
 		return "file/upload_ok";
 	}
--- spring/src/main/webapp/WEB-INF/views/file/upload_form.jsp
+++ spring/src/main/webapp/WEB-INF/views/file/upload_form.jsp
@@ -12,15 +12,15 @@
 	<%-- 파일 업로드 : 클라이언트로부터 파일을 입력받아 서버 디렉토리에 저장하는 기능 --%>
 	<%-- => 파일을 입력받아 전달하기 위한 form 태그를 사용하며 요청방식(method 속성)은
 	반드시 [post]로 설정하고 전달형태(enctype 속성)은 [mulitpart/form-data]로 설정 --%>
-	<form action="upload" method="post" enctype="mulitpart/form-data">
+	<form action="upload" method="post" enctype="multipart/form-data">
 	<table>
 		<tr>
 			<td>업로더 이름</td>
-			<td><input type="text" name="username"></td>
+			<td><input type="text" name="uploaderName"></td>
 		</tr>
 		<tr>
 			<td>업로드 파일</td>
-			<td><input type="file" name="filename"></td>
+			<td><input type="file" name="uploadFile"></td>
 		</tr>
 		<tr>
 			<td colspan="2"><button type="submit">업로드</button></td>
